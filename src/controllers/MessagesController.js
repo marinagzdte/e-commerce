e64113582b1cb6dc070a146ca98be4546f443fdb
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-12-11 19:09:39 -0300

    business

--- src/controllers/MessagesController.js
+++ src/controllers/MessagesController.js
@@ -1,32 +1,41 @@
 import { MessagesRepository } from '../repositories/MessageRepository.js'
+import { isAdmin } from '../middlewares/auth.js';
 import logger from '../utils/logger.js';
 
 const messagesApi = new MessagesRepository();
 
+const getMessages = async (req, res) => {
+    try {
+        let messages;
+        if (isAdmin(req.user)) {
+            messages = await messagesApi.getAll();
+        } else {
+            let aux = await messagesApi.getAll();
+            messages = Array.prototype.concat(aux.filter(msg => msg.to === req.user.email),
+                                              aux.filter(msg => msg.from === req.user.email))
+        }    
+        res.json(messages);
+
+    } catch (error) {
+        logger.logError(error)
+
+        res.status(500);
+        res.json({ error: -1, descripcion: 'error al listar los mensajes' });
+    }
+}
+
 const getMessage = async (req, res) => {
-    if (req.params.id === undefined) {
-        try {
-            const messages = await messagesApi.getAll();
-            res.json(messages);
-        } catch (error) {
-            logger.logError(error)
+    try {
+        const message = await messagesApi.getById(req.params.id);
+        res.json(message);
+    } catch (error) {
+        logger.logError(error);
 
+        if (error.message.includes('404'))
+            res.status(404);
+        else
             res.status(500);
-            res.json({ error: -1, descripcion: 'error al listar los mensajes' });
-        }
-    } else {
-        try {
-            const message = await messagesApi.getById(req.params.id);
-            res.json(message);
-        } catch (error) {
-            logger.logError(error);
-
-            if (error.message.includes('404'))
-                res.status(404);
-            else
-                res.status(500);
-            res.json({ error: -2, descripcion: 'mensaje no encontrado' });
-        }
+        res.json({ error: -2, descripcion: 'mensaje no encontrado' });
     }
 }
 
@@ -72,4 +81,4 @@ const deleteMessage = async (req, res) => {
     }
 }
 
-export { getMessage, postMessage, deleteMessage }
+export { getMessages, getMessage, postMessage, deleteMessage }
