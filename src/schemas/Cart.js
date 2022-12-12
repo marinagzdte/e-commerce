fdc25668f87d7cd863f04b441a19980cc79c45a7
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-12-11 22:24:59 -0300

    remove unused fields

--- src/schemas/Cart.js
+++ src/schemas/Cart.js
@@ -3,7 +3,5 @@ const { Schema } = mongoose;
 
 export const CartSchema = new Schema({
     timestamp: { type: Date, required: true },
-    email: { type: String, required: true},
-    address: { type: String, required: true },
     products: { type: Array, required: true },
 });
